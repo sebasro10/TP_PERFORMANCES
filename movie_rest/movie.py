f671f112e0ddc440e98f0f2b7e0810981de7f6db
Sebastian Romero <sebastian.romero@imt-atlantique.net>
2022-10-14 03:30:34 +0200

    movie - rest

---
+++ movie_rest/movie.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, jsonify, make_response
+import json
+
+app = Flask(__name__)
+
+PORT = 5003
+HOST = '0.0.0.0'
+
+with open('{}/database/movies.json'.format("."), "r") as jsf:
+   movies = json.load(jsf)["movies"]
+
+# root message
+@app.route("/", methods=['GET'])
+def home():
+    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
+
+@app.route("/movies", methods=['GET'])
+def get_movies():
+    res = make_response(jsonify(movies), 200)
+    return res
+
+@app.route("/movies/<movieid>", methods=['GET'])
+def get_movie_byid(movieid):
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            res = make_response(jsonify(movie),200)
+            return res
+    return make_response(jsonify({"error":"Movie ID not found"}),400)
+
+@app.route("/movies/<movieid>", methods=['POST'])
+def create_movie(movieid):
+    req = request.get_json()
+
+    for movie in movies:
+        if str(movie["id"]) == str(movieid):
+            return make_response(jsonify({"error":"movie ID already exists"}),200)
+
+    req["id"] = movieid
+    movies.append(req)
+    res = make_response(jsonify({"message":"movie added"}),200)
+    return res
+
+if __name__ == "__main__":
+    #p = sys.argv[1]
+    print("Server running in port %s"%(PORT))
+    app.run(host=HOST, port=PORT)
